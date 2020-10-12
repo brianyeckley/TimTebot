c6c16f05b8371af2d9d36f80386aa360e9a36505
Brian Yeckley <brian.yeckley@gmail.com>
2020-10-12 17:22:16 -0500

    New advance flow

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const quotes = require("./quotes.json")
 const fs = require('fs');
-const { match } = require("assert");
 
 const prefix = "!";
 const client = new Discord.Client();
+
+// None of this will work with multiple servers or concurrent DM's, idgaf right now
+let advanceState = {
+    completed: true,
+    coachIndex: 0,
+    needsHomeAway: true,
+    needsOpponent: true
+}
 
 client.on("message", function(message) { 
     if (message.author.bot) return;
@@ -16,20 +23,24 @@ client.on("message", function(message) {
         message.channel.send(quote);
     }
 
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix) && message.channel.type != 'dm') return;
+
+    let data = load();
+
+    if (message.channel.type == 'dm') {
+        handleDM(data, message);
+    }
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    let data = load();
-
     if (command === 'help') {
-        let help = `!register - \`Brian Colorado Buffaloes\``;
-        help += "\n!list";
-        help += `\n!result - \`29-14\` (your score first)`;
-        help += `\n!advance - \`Brian Home vs Oregon Ducks | Stew Away vs Georgia State Panthers | ETSweens bye\``;
-        help += "\n!current";
+        let help = `\`!register Brian Colorado Buffaloes\``;
+        help += "\n\`!list\`";
+        help += "\n\`!result - 29-14\` (your score first)";
+        help += "\n\`!advance\` and we'll have a chat";
+        help += "\n\`!current\`";
         help += "\n\nThank *you* for using TimTebot!";
 
         message.channel.send(help);
@@ -68,42 +79,16 @@ client.on("message", function(message) {
         data.Results.push({ Coach: player.Name, Score: score, Opponent: args.join(' ') });
     }
     else if (command === 'advance') {
-        // Format "Brian Home vs Oregon Ducks | Stew Away vs Georgia State Panthers | ETSweens bye"
-        const fullArgs = args.join(' ');
-
-        const matchups = fullArgs.split('|');
-
-        data.CurrentWeek = [];
-
-        matchups.forEach(matchup => {
-            let matchupArgs = matchup.split(' ');
-
-            matchupArgs = matchupArgs.filter(function (el) {
-                return el.length > 0;
-            });
-
-            const player = data.Players.find(player => player.Name === matchupArgs[0]);
-
-            const homeAwayBye = matchupArgs[1];
-
-            if (homeAwayBye.toLowerCase() === 'bye') {
-                data.CurrentWeek.push( { Coach: player.Name, Home: homeAwayBye, Opponent: 'Bye' } );
-            } else {
-                // Remove coach name and home vs away and 'vs'
-                matchupArgs.shift();
-                matchupArgs.shift();
-                matchupArgs.shift();
-
-                const opponent = matchupArgs.join(' ');
-
-                data.CurrentWeek.push( { Coach: player.Name, Home: homeAwayBye, Opponent: opponent });
-            }
-        });
+        advance(data, message);
 
         outputGames(data, message);
     }
     else if (command === 'current') {
         outputGames(data, message);
+    }
+    else if (command == 'shutdown') {
+        message.reply('BYE :middle_finger:');
+        process.exit();
     }
 
     save(data);
@@ -136,4 +121,89 @@ const outputGames = (data, message) => {
             message.channel.send(`${game.Coach} ${atOrVs} ${game.Opponent}`);
         }
     });
+}
+
+const handleDM = (data, message) => {
+    console.log(`received dm from ${message.author}`);
+    console.log(advanceState);
+    if (advanceState.completed) {
+        message.reply('sup');
+        return;
+    }
+
+    if (advanceState.needsHomeAway) {
+        const player = data.Players[advanceState.coachIndex];
+
+        if (message.content.toLowerCase() === 'home') {
+            advanceState.needsHomeAway = false;
+            advanceState.game.Home = 'home';
+        }
+        else if (message.content.toLowerCase() === 'away') {
+            advanceState.needsHomeAway = false;
+            advanceState.game.Home = 'away';
+        }
+        else if (message.content.toLowerCase() === 'bye') {
+            advanceState.needsHomeAway = false;
+            advanceState.needsOpponent = false;
+            advanceState.game.Home = 'bye';
+            teamAdvanced(data, message);
+            return;
+        }
+        else {
+            message.reply('Invalid option, please say \`home\`, \`away\`, or \`bye\`');
+        }
+
+        if (!advanceState.needsHomeAway) {
+            // Successfully got home/away
+            message.reply(`Thanks, now who is ${player.Name} playing this week?`);
+        }
+    } 
+    else if (advanceState.needsOpponent) {
+        advanceState.needsOpponent = false;
+        advanceState.game.Opponent = message.content;
+        teamAdvanced(data, message);
+    }
+}
+
+const teamAdvanced = (data, message) => {
+    const player = data.Players[advanceState.coachIndex];
+
+    data.CurrentWeek.push( { Coach: player.Name, Home: advanceState.game.Home, Opponent: advanceState.game.Opponent });
+
+    const nextIndex = advanceState.coachIndex += 1;
+
+    if (data.Players.length == nextIndex) {
+        advanceState.completed = true;
+        message.reply('Week advanced!');
+        return;
+    }
+
+    advanceState = {
+        completed: false,
+        coachIndex: nextIndex,
+        needsHomeAway: true,
+        needsOpponent: true,
+        game: {}
+    }
+
+    const nextPlayer = data.Players[advanceState.coachIndex];
+
+    message.reply(`Next up, coach ${nextPlayer.Name}. Are they \`home\`, \`away\`, or on a \`bye\`?`);
+}
+
+const advance = (data, message) => {
+    console.log(`received advance request from ${message.author}`);
+    data.CurrentWeek = [];
+
+    advanceState = {
+        completed: false,
+        coachIndex: 0,
+        needsHomeAway: true,
+        needsOpponent: true,
+        game: {}
+    }
+
+    const firstPlayer = data.Players[0];
+
+    message.author.send(`First up, coach ${firstPlayer.Name}. Are they \`home\`, \`away\`, or on a \`bye\`?`);
 }
